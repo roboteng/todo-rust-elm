ae5227687335bb1c0c2d2e86503b6212d4739aea
Trevor Settles <roboteng@gmail.com>
2025-06-02 20:00:20 -0600

    copied example directly, `.into_make_service_with_connect_info::<SocketAddr>()`

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,87 @@
+//! Example websocket server.
+//!
+//! Run the server with
+//! ```not_rust
+//! cargo run -p example-websockets --bin example-websockets
+//! ```
+//!
+//! Run a browser client with
+//! ```not_rust
+//! firefox http://localhost:3000
+//! ```
+//!
+//! Alternatively you can run the rust client (showing two
+//! concurrent websocket connections being established) with
+//! ```not_rust
+//! cargo run -p example-websockets --bin example-client
+//! ```
+
 use axum::{
     Router,
     body::Bytes,
-    extract::{
-        ConnectInfo,
-        ws::{CloseFrame, Message, Utf8Bytes, WebSocket, WebSocketUpgrade},
-    },
+    extract::ws::{Message, Utf8Bytes, WebSocket, WebSocketUpgrade},
     response::IntoResponse,
     routing::any,
 };
 use axum_extra::{TypedHeader, headers};
-use futures_util::SinkExt;
-use futures_util::StreamExt;
 
 use std::ops::ControlFlow;
 use std::{net::SocketAddr, path::PathBuf};
-use tower_http::services::ServeDir;
+use tower_http::{
+    services::ServeDir,
+    trace::{DefaultMakeSpan, TraceLayer},
+};
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+//allows to extract the IP of connecting user
+use axum::extract::connect_info::ConnectInfo;
+use axum::extract::ws::CloseFrame;
+
+//allows to split the websocket stream into separate TX and RX branches
+use futures_util::{sink::SinkExt, stream::StreamExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
 
+    // build our application with some routes
     let app = Router::new()
         .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
-        .route("/ws", any(ws_handler));
+        .route("/ws", any(ws_handler))
+        // logging so we can see what's going on
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        );
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("localhost:3000")
+    // run it with hyper
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
-    println!("Serving on: http://localhost:3000/");
-    axum::serve(listener, app).await.unwrap();
+    tracing::debug!("listening on http://{}", listener.local_addr().unwrap());
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
 
+/// The handler for the HTTP request (this gets called when the HTTP request lands at the start
+/// of websocket negotiation). After this completes, the actual switching from HTTP to
+/// websocket protocol will occur.
+/// This is the last point where we can extract TCP/IP metadata such as IP address of the client
+/// as well as things from HTTP headers such as user-agent of the browser etc.
 async fn ws_handler(
     ws: WebSocketUpgrade,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
@@ -48,7 +98,7 @@ async fn ws_handler(
     ws.on_upgrade(move |socket| handle_socket(socket, addr))
 }
 
-/// Actual websocket state machine (one will be spawned per connection)
+/// Actual websocket statemachine (one will be spawned per connection)
 async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
     // send a ping (unsupported by some browsers) just to kick things off and get a response
     if socket
